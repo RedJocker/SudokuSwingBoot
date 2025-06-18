161ddb313066d15b9246975cb694ccb9d9d96489
RedJocker <mauricio.guaruja@gmail.com>
2025-06-18 12:40:00 -0300

    run console application

--- src/main/java/org/play/sudokuSwingBoot/SudokuSwingBootApplication.java
+++ src/main/java/org/play/sudokuSwingBoot/SudokuSwingBootApplication.java
@@ -2,6 +2,7 @@ package org.play.sudokuSwingBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class SudokuSwingBootApplication implements CommandLineRunner {
@@ -9,5 +10,8 @@ public class SudokuSwingBootApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SudokuSwingBootApplication.class, args);
 	}
-	
+
+	public void run(String[] args) {
+		System.out.println("HELLO!!!");
+	}
 }
