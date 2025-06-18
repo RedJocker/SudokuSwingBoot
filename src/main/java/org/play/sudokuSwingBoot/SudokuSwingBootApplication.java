d492ebafbb2a8e753e3759f93a2d00b612b932fd
RedJocker <mauricio.guaruja@gmail.com>
2025-06-18 12:23:04 -0300

    init project

---
+++ src/main/java/org/play/sudokuSwingBoot/SudokuSwingBootApplication.java
@@ -0,0 +1,13 @@
+package org.play.sudokuSwingBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuSwingBootApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuSwingBootApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/org/play/sudokuSwingBoot/SudokuSwingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.play.sudokuSwingBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuSwingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
