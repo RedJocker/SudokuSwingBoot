be48d106499ca587b44498760abf362b986a9760
RedJocker <mauricio.guaruja@gmail.com>
2025-06-18 16:13:27 -0300

    pass args to SudokuView

--- src/main/java/org/play/sudokuSwingBoot/SudokuSwingBootApplication.java
+++ src/main/java/org/play/sudokuSwingBoot/SudokuSwingBootApplication.java
@@ -9,17 +9,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class SudokuSwingBootApplication implements CommandLineRunner {
+public class SudokuSwingBootApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SudokuSwingBootApplication.class)
 			.headless(false)
 			.web(WebApplicationType.NONE)
 			.run(args);
-	}
-
-	public void run(String[] args) {
-		System.out.println("HELLO!!!");
-		System.out.println(Arrays.toString(args));
 	}
 }
--- src/main/java/org/play/sudokuSwingBoot/gui/SudokuView.java
+++ src/main/java/org/play/sudokuSwingBoot/gui/SudokuView.java
@@ -1,10 +1,12 @@
 package org.play.sudokuSwingBoot.gui;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
 import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
+import java.util.Arrays;
 
 @Component
 public class SudokuView extends JFrame {
@@ -14,9 +16,14 @@ public class SudokuView extends JFrame {
 
 	public SudokuView(
 		final BorderLayout borderLayout,
-		final SudokuTitle sudokuTitle
+		final SudokuTitle sudokuTitle,
+		ApplicationArguments args
 	) {
+		
 		System.out.println("HELLO SUDOKU VIEW");
+		System.out.println(
+			Arrays.toString(args.getSourceArgs())
+		);
 		this.setTitle("Sudoku Swing Boot");
 		this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
