1dc11d2f751302dda8381de643eb88641fbe941a
RedJocker <mauricio.guaruja@gmail.com>
2025-06-21 13:59:08 -0300

    Fix: running script without args does not error out

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-_JAVA_AWT_WM_NONREPARENTING=1 exec ./gradlew bootRun --args="$*"
+ARGS=""
+if [ $# -gt 0 ]; then
+    ARGS=--args="$*"
+fi
+
+_JAVA_AWT_WM_NONREPARENTING=1 exec ./gradlew bootRun $ARGS
