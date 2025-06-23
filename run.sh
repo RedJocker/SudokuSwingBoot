5430cc66b354b1bdd8367c25994de53ae6ac9586
redjocker <mauricio.guaruja@gmail.com>
2025-06-23 12:59:58 -0300

    Fix: first run after cloning does not have gradlew available

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-if [ $# -gt 0 ]; then
-    _JAVA_AWT_WM_NONREPARENTING=1 exec ./gradlew bootRun --args="$*"
+# if first run gradlew is not available
+if [ -f "./gradlew" ]; then
+    GRADLE_CMD="./gradlew"
+else
+    GRADLE_CMD="gradle"
 fi
 
- _JAVA_AWT_WM_NONREPARENTING=1 exec ./gradlew bootRun
-
+if [ $# -gt 0 ]; then
+    _JAVA_AWT_WM_NONREPARENTING=1 exec $GRADLE_CMD bootRun --args="$*"
+else
+    _JAVA_AWT_WM_NONREPARENTING=1 exec $GRADLE_CMD bootRun
+fi
